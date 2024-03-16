be13aa03192b6dbbb39f30acd62c5869df9550d3
Jiyeon Kim <jykim3994@gmail.com>
2024-03-16 21:22:36 +0900

    second commit

--- 2D_DHIT/2D DHIT DNS.py
+++ 2D_DHIT/2D DHIT DNS.py
@@ -14,8 +14,7 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=log
 import math
 pi = math.pi; exp = np.exp; log = np.log
 
-from pesudo_spectral import *
-from statistics import *
+from pesudo_spectral import nonlinear_wave
 
 Data_Dir = 'D:/Research/00_Data/2D_DHIT/resolution 128'; os.makedirs(Data_Dir, exist_ok = True)
 Train_Dir = Data_Dir + '/Training data'; os.makedirs(Train_Dir, exist_ok = True)
@@ -40,6 +39,7 @@ KY,KX = cp.meshgrid(ky, kx, indexing='ij')
 k_sq = KX**2 + KY**2
 k_round = cp.round(cp.sqrt(k_sq)).astype(np.int16); k_max = cp.max(k_round)
 k_index, k_count = cp.unique(k_round, return_counts = True)
+k_sq[0, 0] = 1
 
 # Physical parameters
 nu = 1.0e-3  # Kinematic viscosity (adjust as needed)
@@ -47,30 +47,27 @@ nu = 1.0e-3  # Kinematic viscosity (adjust as needed)
 
 
 
-logging.info(f"{num_train} number of simulations using C-N for viscous term/ 2nd A-B for nonlinear term \n")
-
 num_train,num_val,num_test = 500,100,50
-# Time-stepping loop
+logging.info(f"{num_train} number of simulations using C-N for viscous term/ 2nd A-B for nonlinear term")
 for num in range(num_train):
-    w0 = cp.random.normal(loc = 0.0, scale = 1.0, size = (ny,nx))
+    w0 = cp.random.normal(loc = 0.0, scale = 1.0, size = (Ny,Nx))
     wk = rfftn(w0, axes = (0,1), norm = 'forward')
     wk = wk/(cp.abs(wk) + 10**-5) # Make amplitudes of all modes to one. 10**-5 prevents dividing by zero.
     wk = ((cp.abs(k_sq - 10.0) + 1)**-2.0)*wk; wk[0,0] = 0
-    
     w0 = irfftn(wk, axes = (0,1), norm = 'forward').real; w = np.expand_dims(cp.asnumpy(w0), axis = (0,1))
     
+    # Time-stepping loop
     for q in range(Nt):
         Jk, uk, vk = nonlinear_wave(wk)
-        if q == 0: Jkp = Jk[:] # 이 부분 id 겹치는지 확인 필요
-        
+        if q == 0: Jkp = Jk[:]
         wk = ((1 - 0.5*nu*k_sq*dt)*wk - dt*(1.5*Jk - 0.5*Jkp))/(1 + 0.5*nu*k_sq*dt); wk[0,0] = 0
-        Jkp[] = Jk[]
+        Jkp[:] = Jk[:]
 
-        if q % K == 0:
-            wn = irfftn(wk, axes = (0,1), norm = 'forward'); wn = np.expand_dims(cp.asnumpy(wn), axis = (0,1))
+        if (q + 1) % K == 0:
+            wn = irfftn(wk, axes = (0,1), norm = 'forward').real; wn = np.expand_dims(cp.asnumpy(wn), axis = (0,1))
             w = np.concatenate((w,wn), axis = 0)
     
-    CreateData = os.path.join(Train_Dir, f"/Train{num+1} 2D HIT n={N} T={T} dt={dt:.4f} K={K} data.h5")
+    CreateData = os.path.join(Train_Dir, f"Training{num+1} 2D HIT n={N} T={T} dt={dt:.4f} K={K} data.h5")
     fc = h5py.File(CreateData, 'w')
     fc.create_dataset('w', data = w) # Keyword of data
     fc.close()
--- 2D_DHIT/pesudo_spectral.py
+++ 2D_DHIT/pesudo_spectral.py
@@ -6,20 +6,46 @@ if torch.cuda.is_available():
 else:
     from numpy.fft import rfftn, irfftn
     print("GPU acceleration is not available. Change all cp's into np's.")
-    
+import math
+pi = math.pi; exp = np.exp; log = np.log
+
+# Parameters
+a = 0; b = 2*pi; L = b - a # Box size
+N = 128; Nx,Ny = N,N  # Grid resolution
+Mx,My = 3*Nx//2,3*Ny//2
+dt = 0.0025; T = 3*10**1; Nt = int(T/dt); K = 20  # Time step
+
+# Grid and wavenumbers
+x = cp.linspace(a, b, Nx, endpoint = False, dtype = np.float64)
+y = cp.linspace(a, b, Ny, endpoint = False, dtype = np.float64)
+Y,X = cp.meshgrid(y, x, indexing='ij')
+
+kx = cp.fft.rfftfreq(Nx, 1/Nx)
+ky = cp.fft.fftfreq(Ny, 1/Ny)
+KY,KX = cp.meshgrid(ky, kx, indexing='ij')
+
+k_sq = KX**2 + KY**2
+k_round = cp.round(cp.sqrt(k_sq)).astype(np.int16); k_max = cp.max(k_round)
+k_index, k_count = cp.unique(k_round, return_counts = True)
+k_sq[0, 0] = 1 
+
+# Physical parameters
+nu = 1.0e-3  # Kinematic viscosity (adjust as needed)
+# rho =
+
 #pesudo-spectral.py
 def zero_cat(X):
-    pad = cp.zeros_like((My,Mx//2+1), dtype = np.complex128)
-    X = cp.concatenate((X[:,:],pad[:Ny,-Nx//2:]), axis = 1) # (Mx//2 + 1) - (Nx//2 + 1)
+    pad = cp.zeros((My,Mx//2+1), dtype = np.complex128)
+    X = cp.concatenate((X[:,:],pad[:Ny,-Nx//4:]), axis = 1) # (Mx//2 + 1) - (Nx//2 + 1)
     X = cp.concatenate((X[:Ny//2,:],pad[Ny//2:-Ny//2,:],X[Ny//2:,:]), axis = 0)
     return X
 
 def zero_padding(X):
-    X[:,-Nx//2:] = 0; X[Ny//2:-Ny//2,:] = 0
+    X[:,-Nx//4:] = 0; X[Ny//2:-Ny//2,:] = 0
     return X
 
 def removing_interpolation(X):
-    X = cp.concatenate((X[:Ny//2,:-Nx//2],X[-Ny//2:,:-Nx//2]), axis = 0)
+    X = cp.concatenate((X[:Ny//2,:-Nx//4],X[-Ny//2:,:-Nx//4]), axis = 0)
     return X
 
 def nonlinear_wave(wk): # Pesudo-spectral method with 3/2 zero-padding
@@ -31,5 +57,5 @@ def nonlinear_wave(wk): # Pesudo-spectral method with 3/2 zero-padding
     vk_pad = zero_cat(vk); v_pad = irfftn(vk_pad, axes = (0,1), norm = 'forward').real
     
     convective_term = u_pad*w_x_pad + v_pad*w_y_pad
-    convective_term_wave = rttfn(convective_term, axes = (0,1), norm = 'forward')
+    convective_term_wave = rfftn(convective_term, axes = (0,1), norm = 'forward')
     return removing_interpolation(convective_term_wave), uk, vk
